4fb5788cc28c30be79b08cdaa6b33306465670a5
hamid3311khan-hub <hamid3311khan@gmail.com>
2026-07-30 15:13:54 +0530

    Update pages.js

--- routes/pages.js
+++ routes/pages.js
@@ -1,15 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const path = require('path');
 
-module.exports = (io) => {  // <-- ye line add ki
-  router.get('/customer-register', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/customer-register.html'));
-  });
-
-  router.get('/', (req, res) => {
-    res.send('QuickBite API Running ✅');
-  });
-
-  return router; // <-- ye bhi important
-}
+module.exports = (io) => {
+  router.get('/about', (req,res) => res.json({page: "About Us"}))
+  router.get('/terms', (req,res) => res.json({page: "Terms & Conditions"}))
+  return router;
+};
