1451c7e80b47b4c8db1aa512de78aae150ae6103
hamid3311khan-hub <hamid3311khan@gmail.com>
2026-07-30 11:47:22 +0530

    Update pages.js

--- routes/pages.js
+++ routes/pages.js
@@ -2,12 +2,8 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-router.get('/', (req,res)=> res.sendFile(path.join(__dirname, '../public', 'index.html')));
-router.get('/admin', (req,res)=> res.sendFile(path.join(__dirname, '../public', 'admin.html')));
-router.get('/restaurant-dashboard', (req,res)=> res.sendFile(path.join(__dirname, '../public', 'restaurant-dashboard.html')));
-router.get('/restaurant-register', (req,res)=> res.sendFile(path.join(__dirname, '../public', 'restaurant-register.html')));
-router.get('/restaurant-login', (req,res)=> res.sendFile(path.join(__dirname, '../public', 'restaurant-login.html')));
-router.get('/rider-register', (req,res)=> res.sendFile(path.join(__dirname, '../public', 'rider-register.html')));
-router.get('/rider-login', (req,res)=> res.sendFile(path.join(__dirname, '../public', 'rider-login.html')));
+router.get('/customer-register', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/customer-register.html'));
+});
 
 module.exports = router;
