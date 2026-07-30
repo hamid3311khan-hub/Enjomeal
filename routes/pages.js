ded1a0920e6da100aa95bf74270dd0578163c902
hamid3311khan-hub <hamid3311khan@gmail.com>
2026-07-30 17:44:09 +0530

    Delete pages.js

--- routes/pages.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-module.exports = (io) => {
-  router.get('/about', (req,res) => res.json({page: "About Us"}))
-  router.get('/terms', (req,res) => res.json({page: "Terms & Conditions"}))
-  return router;
-};
