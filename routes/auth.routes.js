d9a15d0ec269bd28b5881c157249485714f6c5e2
hamid3311khan-hub <hamid3311khan@gmail.com>
2026-08-03 09:06:08 +0530

    Delete routes/auth.routes.js

--- routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Temp route
-router.post('/login', (req, res) => {
-  res.json({ success: true, message: "Login API working" });
-});
-
-module.exports = router;
