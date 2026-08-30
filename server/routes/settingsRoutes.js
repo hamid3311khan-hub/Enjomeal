cbceae8b5750d253374b6fdcae59643c9e0de6a2
hamid3311khan-hub <hamid3311khan@gmail.com>
2026-08-30 18:55:39 +0530

    Fix settings middleware path

--- server/routes/settingsRoutes.js
+++ server/routes/settingsRoutes.js
@@ -5,7 +5,7 @@ const {
   updateSettingsController,
 } = require("../controllers/settingsController");
 
-const authMiddleware = require("../middlewares/auth.middleware");
+const authMiddleware = require("../middleware/auth.middleware");
 
 const router = express.Router();
 
