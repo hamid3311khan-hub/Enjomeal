a94a57a538c376bc4be402c270f44370b79191f6
hamid3311khan-hub <hamid3311khan@gmail.com>
2026-09-16 03:13:12 +0530

    Update App.jsx

--- apps/delivery-panel/src/App.jsx
+++ apps/delivery-panel/src/App.jsx
@@ -2,6 +2,7 @@ import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import DeliveryRegister from "./pages/DeliveryRegister";
 import DeliveryReports from "./pages/DeliveryReports";
+import DeliveryNotifications from "./pages/DeliveryNotifications";
 import "./App.css";
 
 function App() {
@@ -9,6 +10,10 @@ function App() {
 
   if (path === "/delivery/dashboard") {
     return <Dashboard />;
+  }
+
+  if (path === "/delivery/notifications") {
+  return <DeliveryNotifications />;
   }
 
   if (path === "/delivery/reports") {
