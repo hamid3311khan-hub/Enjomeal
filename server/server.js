84d645ac0198b5aa7ebac583014bcd6dbf8d83d3
hamid3311khan-hub <hamid3311khan@gmail.com>
2026-09-19 01:10:52 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -41,6 +41,7 @@ const httpServer = http.createServer(app);
 const socketOrigins = [
   "https://enjomeal-customer-web.onrender.com",
   "https://enjomeal.in",
+  "https://restaurant.enjomeal.in",
   ...(process.env.CLIENT_URL
     ? process.env.CLIENT_URL
         .split(",")
@@ -290,6 +291,7 @@ app.use(requestIdMiddleware);
 const allowedOrigins = [
   "https://enjomeal-customer-web.onrender.com",
   "https://enjomeal.in",
+  "https://restaurant.enjomeal.in",
   ...(process.env.CLIENT_URL
     ? process.env.CLIENT_URL
         .split(",")
